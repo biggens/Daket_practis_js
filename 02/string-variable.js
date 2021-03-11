8c0a0233c545ed3a6ded002276ea77efc2e3cb57
Volchik_Ihar <biggens.man@gmail.com>
2021-03-11 19:02:33 +0300

    add task 02

---
+++ 02/string-variable.js
@@ -0,0 +1,9 @@
+let username;
+let message;
+username = 'Катерина';
+message = 'Взгляни на мир';
+
+let elName = document.getElementById ('name');
+elName.textContent = username;
+let elNote = document.getElementById('note');
+elNote.textContent = message;
